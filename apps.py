7756b48cfeaab3ca0329681cc36d469e35ad205b
Daniel florez <florezx15@gmail.com>
2025-05-16 12:37:42 -0500

    Delete media folders & form exceptions refact
    Form exceptions now follow django best practices.
    If the record is disabled and then delete it, django will remove the media files related.

--- apps.py
+++ apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ImageMngtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'image_mngt'
+    
+    def ready(self):
+        from .signals import handle_folder_delete
